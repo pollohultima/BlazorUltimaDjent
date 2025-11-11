7db7632c2a44c24685c81679c625638b08b86d6a
pollohultima <matteo.polonio@gmail.com>
2025-11-11 21:12:02 +0100

    Commit

--- Models/CardModel.cs
+++ Models/CardModel.cs
@@ -2,5 +2,7 @@ namespace BlazorUltimaDjent.Models;
 
 public class CardModel
 {
-    
+    public string? Title { get; set; }
+    public double Price { get; set; }
+    public string? ImageSource { get; set; }
 }
